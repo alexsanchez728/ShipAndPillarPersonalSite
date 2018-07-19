fec3548f6c16f1766b941d4d5499f7685f77f668
Alex Sanchez <alesanchez728@gmail.com>
2018-07-19 16:58:19 -0500

    seperate js file for portfolio page

---
+++ javascripts/portfolio.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$('#port-box-1').mousemove(onBoxHover);
+$('#port-box-2').mousemove(onBoxHover);
+$('#port-box-3').mousemove(onBoxHover);
+$('#port-box-4').mousemove(onBoxHover);
+
+
+var onBoxHover = () => {
+  console.log("here");
+};
